7bcc2f672bc5f808e151b62fcf90a16f0d4c0daa
netw0rk7 <dinnapas.ncr@gmail.com>
2025-11-14 11:40:23 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://netw0rk7.github.io',
+})
